d95310106bf0bf36b4bcfb75237ec99f6f557d7a
limpo-iori <385606406@qq.com>
2019-10-11 12:33:42 +0800

    python 小技巧

--- python_test.py
+++ python_test.py
@@ -20,3 +20,24 @@ print(a,b)
 # 字符切片
 print('hello world'.split())
 print('hello,world'.split(','))
+
+# 异常处理
+try:
+	print(1/1)
+except ZeroDivisionError as e:
+	print('error: division zero')
+else:
+	print('normal')
+finally:
+	print('completely')
+
+# 列表随机采样
+import random
+red_ball = [x+1 for x in range(33)]
+bull_ball = [x+1 for x in range(12)]
+for i in range(10):
+	red_ball_list = random.sample(red_ball,5)
+	bull_ball_list = random.sample(bull_ball,2)
+	red_ball_list.sort()
+	bull_ball_list.sort()
+	print(red_ball_list,bull_ball_list)
