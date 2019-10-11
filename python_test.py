f9d0cc18474892b1e0f457720d5da584be453acd
limpo-iori <385606406@qq.com>
2019-10-11 10:47:39 +0800

    python小技巧

---
+++ python_test.py
@@ -0,0 +1,22 @@
+# 字符串倒序
+s1 = 'hello word'
+print(s1[::-1])
+
+# 标题首字母大写
+print(s1.title())
+
+# 去除重复字符
+s2 = 'aabbccddeeff'
+print("".join(set(s2)))
+
+# 列表生成
+print([x for x in range(10)])
+
+# 数值交换
+a,b = 10,20
+a,b = b,a
+print(a,b)
+
+# 字符切片
+print('hello world'.split())
+print('hello,world'.split(','))
